3581f36830a26952024b63ab6c763c76ba31a633
Prasann Parab <prasannparab.devops@gmail.com>
2026-09-03 23:32:07 +0530

    Move Maven source to repository root

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException {
+
+        response.getWriter().println("Hello from Maven WAR project!");
+    }
+}
